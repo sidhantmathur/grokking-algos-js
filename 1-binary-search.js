c91eaa214f0e90ed98b6ccf6248ff5ada10a6ff4
sidhantmathur <sidhant185@gmail.com>
2021-01-14 13:24:32 -0600

    init setting up binary search

---
+++ 1-binary-search.js
@@ -0,0 +1,10 @@
+// binary search
+
+// cut elements in half, 
+// check if answer is higher or lower, 
+// then cut in half again. 
+
+let low = 0
+let high = list.length - 1
+
+let mid = Math.floor((low + high) / 2)
