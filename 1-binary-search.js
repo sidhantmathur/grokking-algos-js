a902680ca78d1c5ee05f320b9bd276e6f52a573e
sidhantmathur <sidhant185@gmail.com>
2021-01-14 13:36:53 -0600

    setup while loop and test case

--- 1-binary-search.js
+++ 1-binary-search.js
@@ -4,7 +4,27 @@
 // check if answer is higher or lower, 
 // then cut in half again. 
 
-let low = 0
-let high = list.length - 1
+function binarySearch (list, item) {
+  let low = 0
+  let high = list.length - 1
 
-let mid = Math.floor((low + high) / 2)
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2)
+
+    guess = list[mid]
+  
+    if (guess > item) {
+      high = mid - 1
+    } else if (guess < item) {
+      low = mid + 1
+    } else if (guess == item) {
+      return mid
+    } else {
+      console.log("error")
+    }
+  }
+}
+
+const some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+binarySearch(some_list, 3)
