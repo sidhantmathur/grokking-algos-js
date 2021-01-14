5b7bbf8b900714e05a6c9685ac182bd3b692d2a6
sidhantmathur <sidhant185@gmail.com>
2021-01-14 13:46:48 -0600

    define guess var, add console logs to vis guesses

--- 1-binary-search.js
+++ 1-binary-search.js
@@ -1,3 +1,4 @@
+'use strict';
 // binary search
 
 // cut elements in half, 
@@ -10,21 +11,33 @@ function binarySearch (list, item) {
 
   while (low <= high) {
     let mid = Math.floor((low + high) / 2)
+    let guess = list[mid]
 
-    guess = list[mid]
-  
+    console.log('middle index of list is', mid)
+    console.log('computer guesses', guess)
+
     if (guess > item) {
       high = mid - 1
     } else if (guess < item) {
       low = mid + 1
-    } else if (guess == item) {
+    } else if (guess === item) {
       return mid
     } else {
       console.log("error")
     }
   }
+  return null;
 }
 
 const some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-binarySearch(some_list, 3)
+const list2 = [1, 3, 5, 7, 9];
+
+
+console.log('index of guess is', binarySearch(some_list, 3))
+console.log('index of guess is', binarySearch(some_list, 5))
+
+console.log('index of guess is', binarySearch(list2, 3))
+
+// console.log(binarySearch(some_list, 5))
+// console.log(binarySearch(some_list, -1))
